afbc6b2fd7881fe8b7962aa01990c8282d58442e
James Taranto <taranto.james@gmail.com>
2024-03-17 22:06:44 +1100

    fix fw-fanctrl module

--- modules/fw-fanctrl/default.nix
+++ modules/fw-fanctrl/default.nix
@@ -1,4 +1,3 @@
-# based on https://blog.stigok.com/2019/11/05/packing-python-script-binary-nicely-in-nixos.html
 {
   config,
   lib,
@@ -6,36 +5,76 @@
   ...
 }: let
   cfg = config.services.fw-fanctrl;
-  configFile = pkgs.writeText "config.json" (builtins.toJSON config.services.fw-fanctrl.config);
+  settingsFile = pkgs.writeText "config.json" (builtins.toJSON config.services.fw-fanctrl.settings);
 in {
   options.services.fw-fanctrl = with lib; {
     enable = mkEnableOption "fw-fanctrl";
-    config = mkOption {
-      defaultStrategy = mkOption {
-        type = types.str;
-      };
-      strategyOnDischarging = lib.mkOption {
-        type = types.str;
-        default = null;
-      };
-      batteryChargingStatusPath = mkOption {
-        type = types.str;
-        default = "/sys/class/power_supply/BAT1/status";
-      };
-      strategies = mkOption {
-        type = types.attrsOf (types.submodule (
-          {name, ...}: {
-            fanSpeedUpdateFrequency = mkOption {type = types.int;};
-            movingAverageInterval = mkOption {type = types.int;};
-            speedCurve = mkOption {
-              type = types.listOf (types.submodule {
-                temp = mkOption {type = types.int;};
-                speed = {type = types.int;};
-              });
+    settings = mkOption {
+      type = types.submodule {
+        options = {
+          defaultStrategy = mkOption {
+            type = types.str;
+          };
+          strategyOnDischarging = lib.mkOption {
+            type = types.nullOr types.str;
+            default = null;
+          };
+          batteryChargingStatusPath = mkOption {
+            type = types.str;
+            default = "/sys/class/power_supply/BAT1/status";
+          };
+          strategies = mkOption {
+            type = types.attrsOf (
+              types.submodule
+              {
+                options = {
+                  fanSpeedUpdateFrequency = mkOption {type = types.int;};
+                  movingAverageInterval = mkOption {type = types.int;};
+                  speedCurve = mkOption {
+                    type = types.listOf (types.submodule {
+                      options = {
+                        temp = mkOption {type = types.int;};
+                        speed = mkOption {type = types.int;};
+                      };
+                    });
+                  };
+                };
+              }
+            );
+            example = {
+              lazy = {
+                fanSpeedUpdateFrequency = 5;
+                movingAverageInterval = 30;
+                speedCurve = [
+                  {
+                    temp = 0;
+                    speed = 15;
+                  }
+                  {
+                    temp = 50;
+                    speed = 15;
+                  }
+                  {
+                    temp = 65;
+                    speed = 25;
+                  }
+                  {
+                    temp = 70;
+                    speed = 35;
+                  }
+                  {
+                    temp = 75;
+                    speed = 50;
+                  }
+                  {
+                    temp = 85;
+                    speed = 100;
+                  }
+                ];
+              };
             };
-          }
-        ));
-        default = {};
+          };
+        };
       };
     };
   };
@@ -51,7 +90,7 @@ in {
       wantedBy = ["multi-user.target"];
 
       serviceConfig = {
-        ExecStart = "${pkgs.fw-fanctrl}/bin/fanctrl.py --config ${configFile}";
+        ExecStart = "${pkgs.fw-fanctrl}/bin/fanctrl.py --config ${settingsFile}";
         Type = "simple";
       };
     };
